3accd123eff923ba712dc56e0f4146310e8602f8
Tou Yang <yato0501@gmail.com>
2021-02-21 15:27:24 -0600

    added role and policy for codepipeline

---
+++ Terraform/weedchart_codepipeline.tf
@@ -0,0 +1,58 @@
+resource "aws_s3_bucket" "weedchart_codepipeline_bucket" {
+  bucket = "weedchart-codepipeline-bucket"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "weedchart_codepipeline_role" {
+  name = "weedchart-codepipeline-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy" "weedchart_codepipeline_policy" {
+  name = "weedchart-codepipeline-policy"
+  role = aws_iam_role.weedchart_codepipeline_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.weedchart_codepipeline_bucket.arn}",
+        "${aws_s3_bucket.weedchart_codepipeline_bucket.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
