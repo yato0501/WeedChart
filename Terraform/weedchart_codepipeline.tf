36aab6500008207663d2e91a8602dbfab37a9f78
Tou Yang <yato0501@gmail.com>
2021-02-21 20:54:12 -0600

    passed permission state.

--- Terraform/weedchart_codepipeline.tf
+++ Terraform/weedchart_codepipeline.tf
@@ -80,49 +80,49 @@ resource "aws_codestarconnections_connection" "weedchart_codestarconnection" {
 #############################################################
 
 
-# resource "aws_codebuild_project" "weedchart_build" {
-#   badge_enabled  = false
-#   build_timeout  = 60
-#   name           = "WeedChart"
-#   queued_timeout = 480
-#   service_role   = aws_iam_role.weedchart_codepipeline_role.arn
+resource "aws_codebuild_project" "weedchart_build" {
+  badge_enabled  = false
+  build_timeout  = 60
+  name           = "WeedChart"
+  queued_timeout = 480
+  service_role   = aws_iam_role.weedchart_codebuild_service_role.arn
+  description = "Building the spa"
 
+  artifacts {
+    encryption_disabled    = false
+    name                   = "weedchart-build"
+    override_artifact_name = false
+    packaging              = "NONE"
+    type                   = "CODEPIPELINE"
+  }
 
-#   artifacts {
-#     encryption_disabled    = false
-#     name                   = "weedchart-build"
-#     override_artifact_name = false
-#     packaging              = "NONE"
-#     type                   = "CODEPIPELINE"
-#   }
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:2.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = false
+    type                        = "LINUX_CONTAINER"
+  }
 
-#   environment {
-#     compute_type                = "BUILD_GENERAL1_SMALL"
-#     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:2.0"
-#     image_pull_credentials_type = "CODEBUILD"
-#     privileged_mode             = false
-#     type                        = "LINUX_CONTAINER"
-#   }
+  logs_config {
+    cloudwatch_logs {
+      status = "ENABLED"
+    }
 
-#   logs_config {
-#     cloudwatch_logs {
-#       status = "ENABLED"
-#     }
+    s3_logs {
+      encryption_disabled = false
+      status              = "DISABLED"
+    }
+  }
 
-#     s3_logs {
-#       encryption_disabled = false
-#       status              = "DISABLED"
-#     }
-#   }
-
-#   source {
-#    # buildspec           = data.template_file.buildspec.rendered
-#     git_clone_depth     = 0
-#     insecure_ssl        = false
-#     report_build_status = false
-#     type                = "CODEPIPELINE"
-#   }
-# }
+  source {
+   # buildspec           = data.template_file.buildspec.rendered
+    git_clone_depth     = 0
+    insecure_ssl        = false
+    report_build_status = false
+    type                = "CODEPIPELINE"
+  }
+}
 
 ##################################################
 
@@ -131,6 +131,11 @@ resource "aws_s3_bucket" "weedchart_codepipeline_bucket" {
   bucket = "weedchart-codepipeline-bucket"
   acl    = "private"
 }
+
+
+
+
+
 
 resource "aws_iam_role" "weedchart_codepipeline_role" {
   name = "weedchart-codepipeline-role"
@@ -147,6 +152,92 @@ resource "aws_iam_role" "weedchart_codepipeline_role" {
       "Action": "sts:AssumeRole"
     }
   ]
+}
+EOF
+}
+
+resource "aws_iam_role" "weedchart_codebuild_service_role" {
+name = "weedchart-codebuild-service-role"
+assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_cloudwatch_log_group" "weedchart_codebuild" {
+  name = "weedchart-codebuild"
+
+}
+
+resource "aws_iam_role_policy" "weedchart_codebuild_policy" {
+  name = "weedchart-codepipeline-policy"
+  role = aws_iam_role.weedchart_codebuild_service_role.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Resource": [
+                "${aws_cloudwatch_log_group.weedchart_codebuild.arn}",
+                "${aws_cloudwatch_log_group.weedchart_codebuild.arn}/*"
+            ],
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Resource": [
+                "*"
+            ],
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:GetBucketAcl",
+                "s3:GetBucketLocation"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Resource": [
+                "*"
+            ],
+            "Action": [
+                "s3:PutObject",
+                "s3:GetBucketAcl",
+                "s3:GetBucketLocation"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "codebuild:CreateReportGroup",
+                "codebuild:CreateReport",
+                "codebuild:UpdateReport",
+                "codebuild:BatchPutTestCases",
+                "codebuild:BatchPutCodeCoverages"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
 }
 EOF
 }
