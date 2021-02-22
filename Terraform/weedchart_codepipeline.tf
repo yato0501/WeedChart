3116eb01c2e417f9ac4b47ca95f133ff754e587b
Tou Yang <yato0501@gmail.com>
2021-02-21 20:28:50 -0600

    update

--- Terraform/weedchart_codepipeline.tf
+++ Terraform/weedchart_codepipeline.tf
@@ -6,6 +6,7 @@ resource "aws_codepipeline" "weedchart_codepipeline" {
     location = aws_s3_bucket.weedchart_codepipeline_bucket.bucket
     type     = "S3"
 
+##### don't fully understand what this does?
     # encryption_key {
     #   id   = data.aws_kms_alias.s3kmskey.arn
     #   type = "KMS"
@@ -44,7 +45,7 @@ resource "aws_codepipeline" "weedchart_codepipeline" {
       version          = "1"
 
       configuration = {
-        ProjectName = "test"
+        ProjectName = "WeedChart"
       }
     }
   }
@@ -78,6 +79,52 @@ resource "aws_codestarconnections_connection" "weedchart_codestarconnection" {
 
 #############################################################
 
+
+# resource "aws_codebuild_project" "weedchart_build" {
+#   badge_enabled  = false
+#   build_timeout  = 60
+#   name           = "WeedChart"
+#   queued_timeout = 480
+#   service_role   = aws_iam_role.weedchart_codepipeline_role.arn
+
+
+#   artifacts {
+#     encryption_disabled    = false
+#     name                   = "weedchart-build"
+#     override_artifact_name = false
+#     packaging              = "NONE"
+#     type                   = "CODEPIPELINE"
+#   }
+
+#   environment {
+#     compute_type                = "BUILD_GENERAL1_SMALL"
+#     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:2.0"
+#     image_pull_credentials_type = "CODEBUILD"
+#     privileged_mode             = false
+#     type                        = "LINUX_CONTAINER"
+#   }
+
+#   logs_config {
+#     cloudwatch_logs {
+#       status = "ENABLED"
+#     }
+
+#     s3_logs {
+#       encryption_disabled = false
+#       status              = "DISABLED"
+#     }
+#   }
+
+#   source {
+#    # buildspec           = data.template_file.buildspec.rendered
+#     git_clone_depth     = 0
+#     insecure_ssl        = false
+#     report_build_status = false
+#     type                = "CODEPIPELINE"
+#   }
+# }
+
+##################################################
 
 
 resource "aws_s3_bucket" "weedchart_codepipeline_bucket" {
@@ -120,12 +167,19 @@ resource "aws_iam_role_policy" "weedchart_codepipeline_policy" {
         "s3:GetObjectVersion",
         "s3:GetBucketVersioning",
         "s3:PutObject",
-        "s3:PutObjectAcl",
-        "codestar-connections:UseConnection"
+        "s3:PutObjectAcl"
       ],
       "Resource": [
         "${aws_s3_bucket.weedchart_codepipeline_bucket.arn}",
-        "${aws_s3_bucket.weedchart_codepipeline_bucket.arn}/*",
+        "${aws_s3_bucket.weedchart_codepipeline_bucket.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codestar-connections:UseConnection"
+      ],
+      "Resource": [
         "${aws_codestarconnections_connection.weedchart_codestarconnection.arn}"
       ]
     },
@@ -141,3 +195,7 @@ resource "aws_iam_role_policy" "weedchart_codepipeline_policy" {
 }
 EOF
 }
+
+# data "aws_kms_alias" "s3kmskey" {
+#   name = "alias/myKmsKey"
+# }
